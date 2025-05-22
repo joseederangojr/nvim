3ae458e72295b2d777ff8560f43d7c3acbbf3735
Jose Ederango Jr <joseederangojr@gmail.com>
2025-05-23 02:20:52 +0800

    fix: tmux navigator setup not call

--- lua/joseederangojr/plugins/tmux.lua
+++ lua/joseederangojr/plugins/tmux.lua
@@ -1,12 +1,14 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  opts = {},
-  config =function()
-    local map = vim.keymap.set
-    map("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { desc = "nvim tmux navigate [h]left" })
-    map("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { desc = "nvim tmux navigate [j]down" })
-    map("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "nvim tmux navigate [k]up" })
-    map("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "nvim tmux navigate [l]right" })
-
-  end
+  {
+    "alexghergh/nvim-tmux-navigation",
+    opts = {},
+    config = function()
+      require("nvim-tmux-navigation").setup({})
+      local map = vim.keymap.set
+      map("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { desc = "nvim tmux navigate [h]left" })
+      map("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { desc = "nvim tmux navigate [j]down" })
+      map("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "nvim tmux navigate [k]up" })
+      map("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "nvim tmux navigate [l]right" })
+    end,
+  },
 }
